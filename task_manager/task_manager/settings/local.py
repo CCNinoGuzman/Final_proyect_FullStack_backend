00d2f52b76e5cad320d050535e48e129b9cc88b3
Ntata-git <nicogorras@hotmail.com>
2025-05-28 22:01:43 -0500

    Cambios recuperacion

---
+++ task_manager/task_manager/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+from dotenv import load_dotenv
+import os 
+
+
+dot_env_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dot_env_path)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv("DEBUG")
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DARABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = "static/"
