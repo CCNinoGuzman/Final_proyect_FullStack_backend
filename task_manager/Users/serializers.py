cc4496572541ca5cbb3de7709d0d3a6fa05905d8
Andres <andres.torressandovalhotmail.com>
2025-06-12 14:31:19 -0500

    Made user model changes

--- task_manager/Users/serializers.py
+++ task_manager/Users/serializers.py
@@ -3,29 +3,30 @@ from rest_framework import serializers
 from users.models import User
 
 def validate_unique_name(value):
-    if User.objects.filter(name__iexact=value).exists():
+    if value and User.objects.filter(name__iexact=value).exists():
         raise serializers.ValidationError("Ya existe un usuario con el mismo nombre.")
     return value
 
 def validate_unique_email(value):
-    if User.objects.filter(email__iexact=value).exists():
+    if value and User.objects.filter(email__iexact=value).exists():
         raise serializers.ValidationError("Ya existe una cuenta con este correo electrónico asociado.")
     return value
 
 def validate_password(value):
-    if not re.search(r'[A-Z]', value):
-        raise serializers.ValidationError("La contraseña debe contener al menos una letra mayúscula.")
-    if not re.search(r'[!@#$%^&*(),.?\":{}|<>/]', value):
-        raise serializers.ValidationError("La contraseña debe contener al menos un carácter especial.")
-    if not re.search(r'[a-zA-Z]', value) or not re.search(r'\d', value):
-        raise serializers.ValidationError("La contraseña debe contener letras y números.")
+    if value:
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("La contraseña debe contener al menos una letra mayúscula.")
+        if not re.search(r'[!@#$%^&*(),.?\":{}|<>/]', value):
+            raise serializers.ValidationError("La contraseña debe contener al menos un carácter especial.")
+        if not re.search(r'[a-zA-Z]', value) or not re.search(r'\d', value):
+            raise serializers.ValidationError("La contraseña debe contener letras y números.")
     return value
 
 class UserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=100, validators=[validate_unique_name])
-    email = serializers.EmailField(max_length=100, validators=[validate_unique_email])
-    password = serializers.CharField(max_length=20, validators=[validate_password], write_only=True)
-    username = serializers.CharField(required=False)  # Para que puedas autogenerarlo
+    name = serializers.CharField(max_length=100, validators=[validate_unique_name], required=False)
+    email = serializers.EmailField(max_length=100, validators=[validate_unique_email], required=False)
+    password = serializers.CharField(max_length=20, validators=[validate_password], write_only=True, required=False)
+    username = serializers.CharField(required=False)
 
     class Meta:
         model = User
@@ -36,3 +37,12 @@ class UserSerializer(serializers.ModelSerializer):
             validated_data['username'] = validated_data['email'].split('@')[0]
         user = User.objects.create_user(**validated_data)
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
